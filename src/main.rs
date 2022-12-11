807d17b46854f6178e0c44d40b57062fa6b8468d
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2022-12-11 08:45:08 -0500

    Only call back into the VM if we have a callback to run

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,10 @@ use crate::asm::{Assembler};
 /// SDL context
 pub static mut SDL: Option<sdl2::Sdl> = None;
 
-fn main()
+fn run_program(vm: &mut VM)
 {
-    syscalls::init_syscalls();
+    use sdl2::event::Event;
+    use sdl2::keyboard::Keycode;
 
     // Initialize the SDL context
     unsafe {
@@ -27,69 +28,58 @@ fn main()
         SDL = Some(sdl_context);
     }
 
+    let mut event_pump = unsafe {
+        SDL.as_mut().unwrap().event_pump().unwrap()
+    };
+
+    let exit_reason = vm.eval();
+
+    match exit_reason
+    {
+        ExitReason::Exit => {
+            if vm.stack_size() > 0
+            {
+                let ret = vm.pop();
+                println!("return value: {:?}", ret);
+            }
+
+            return;
+        }
+
+        ExitReason::Wait => {
+            // Keep processig events
+        }
+    }
+
+    let mut i = 0;
+    'main_loop: loop {
+
+        // TODO: call back into the VM but only if we have a callback to run
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'main_loop
+                },
+                _ => {}
+            }
+        }
+
+        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn main()
+{
+    syscalls::init_syscalls();
+
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
     if args.len() == 2 {
         let asm = Assembler::new();
         let mut vm = asm.parse_file(&args[1]).unwrap();
-
-
-
-        let mut event_pump = unsafe {
-            SDL.as_mut().unwrap().event_pump().unwrap()
-        };
-
-        use sdl2::event::Event;
-        use sdl2::keyboard::Keycode;
-
-        let mut i = 0;
-        'main_loop: loop {
-
-            let exit_reason = vm.eval();
-
-            match exit_reason
-            {
-                ExitReason::Exit => {
-                    if vm.stack_size() > 0
-                    {
-                        let ret = vm.pop();
-                        println!("return value: {:?}", ret);
-                    }
-
-                    return;
-                }
-
-                ExitReason::Wait => {
-                    // Keep processig events
-                }
-            }
-
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit {..} |
-                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                        break 'main_loop
-                    },
-                    _ => {}
-                }
-            }
-
-            //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        }
-
-
-
-
+        run_program(&mut vm);
     }
-
-
-
-
-
-
-
-
-
-
 }
