e79b30b71f7b3c3bbd7eb03893ac03405771a8d0
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2022-12-27 19:02:28 -0500

    Handle local variables in symbol resolution

--- ncc/symbols.rs
+++ ncc/symbols.rs
@@ -2,18 +2,36 @@ use std::collections::HashMap;
 use crate::ast::*;
 use crate::parser::{ParseError};
 
+#[derive(Default)]
+struct Scope
+{
+    decls: HashMap<String, Decl>,
+
+    /// Next local slot index to assign
+    next_idx: usize,
+}
+
 /// Represent an environment with multiple levels of scoping
 #[derive(Default)]
 struct Env
 {
-    scopes: Vec<HashMap<String, Decl>>,
+    scopes: Vec<Scope>,
+
+    /// Number of local slots needed in the function
+    num_locals: usize,
 }
 
 impl Env
 {
     fn push_scope(&mut self)
     {
-        self.scopes.push(HashMap::default());
+        let mut new_scope = Scope::default();
+
+        if self.scopes.len() > 0 {
+            new_scope.next_idx = self.scopes[0].next_idx;
+        }
+
+        self.scopes.push(new_scope);
     }
 
     fn pop_scope(&mut self)
@@ -21,21 +39,45 @@ impl Env
         self.scopes.pop();
     }
 
+    /// Define a new local variable in the topmost scope
+    fn define_local(&mut self, name: &str, var_type: Type)
+    {
+        let num_scopes = self.scopes.len();
+        let top_scope = &mut self.scopes[num_scopes - 1];
+        assert!(top_scope.decls.get(name).is_none());
+
+        let decl = Decl::Local {
+            idx: top_scope.next_idx,
+            t: var_type,
+        };
+
+        top_scope.next_idx += 1;
+        if top_scope.next_idx > self.num_locals {
+            self.num_locals = top_scope.next_idx;
+        }
+
+        top_scope.decls.insert(name.to_string(), decl);
+    }
+
+    /// Define a new variable in the topmost scope
     fn define(&mut self, name: &str, decl: Decl)
     {
         let num_scopes = self.scopes.len();
         let top_scope = &mut self.scopes[num_scopes - 1];
-        top_scope.insert(name.to_string(), decl);
+        assert!(top_scope.decls.get(name).is_none());
+
+        top_scope.decls.insert(name.to_string(), decl);
     }
 
     fn lookup(&self, name: &str) -> Option<Decl>
     {
         let top_idx = self.scopes.len() - 1;
 
-        for idx in top_idx..=0 {
+        for idx in (0..=top_idx).rev() {
+
             let scope = &self.scopes[idx];
 
-            if let Some(decl) = scope.get(name) {
+            if let Some(decl) = scope.decls.get(name) {
                 return Some(decl.clone());
             }
         }
@@ -78,6 +120,8 @@ impl Function
 
         env.pop_scope();
 
+        self.num_locals = env.num_locals;
+
         Ok(())
     }
 }
@@ -115,14 +159,13 @@ impl Stmt
                 incr_expr: Expr,
                 body_stmt: Box<Stmt>,
             },
-
-            /// Local variable declaration
-            VarDecl {
-                var_type: Type,
-                var_name: String,
-                init_expr: Expr,
-            }
             */
+
+            // Local variable declaration
+            Stmt::VarDecl { var_type, var_name, init_expr } => {
+                init_expr.resolve_syms(env)?;
+                env.define_local(var_name, var_type.clone());
+            }
 
             Stmt::Block(stmts) => {
                 env.push_scope();
@@ -149,23 +192,22 @@ impl Expr
             Expr::Int(_) | Expr::String(_) => {}
 
             Expr::Ident(name) => {
+                //dbg!(&name);
+                let decl = env.lookup(name).unwrap();
+                *self = Expr::Ref(decl);
+            }
 
+            Expr::Unary { op, child } => {
+                child.as_mut().resolve_syms(env)?;
+            },
 
+            Expr::Binary { op, lhs, rhs } => {
+                lhs.as_mut().resolve_syms(env)?;
+                rhs.as_mut().resolve_syms(env)?;
             }
 
             /*
-            Unary {
-                op: UnOp,
-                child: Box<Expr>,
-            },
-
-            Binary {
-                op: BinOp,
-                lhs: Box<Expr>,
-                rhs: Box<Expr>,
-            },
-
-            Call {
+            Expr::Call {
                 callee: Box<Expr>,
                 args: Vec<Expr>,
             }
@@ -201,7 +243,12 @@ mod tests
     fn basics()
     {
         parse_ok("void main() {}");
-        parse_ok("void foo(u64 argc) {}");
+        parse_ok("void foo(u64 a) {}");
+        parse_ok("u64 foo(u64 a) { return a; }");
+
+        // Local variables
+        parse_ok("void main() { u64 a = 0; }");
+        parse_ok("void main(u64 a) { u64 a = 0; }");
     }
 
 
