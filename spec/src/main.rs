838d7278235c9f2795f9639af4f51fa7334441f2
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-01-14 10:38:55 -0500

    Initial commit for spec project

---
+++ spec/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_parens)]
+#![allow(unused_mut)]
+
+//mod parser;
+
+use std::env;
+//use parser::*;
+
+fn main()
+{
+    //let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+
+
+
+
+}
