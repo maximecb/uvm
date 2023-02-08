0cf66e7cd50d6d75eaaf5d7fbcbe0c999a9a00e7
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-02-07 21:19:15 -0500

    Move rand/srand to stdlib.h

--- ncc/examples/random.c
+++ ncc/examples/random.c
@@ -1,52 +1,26 @@
+#include <stdlib.h>
 #include <uvm/syscalls.h>
-
-// The seed must be an odd number
-u64 cur_rand = 1337;
-
-// Multiplicative Congruential Generator (MCG)
-// Constant from "Computationally Easy, Spectrally Good Multipliers for
-// Congruential Pseudorandom Number Generators" by Steele & Vigna.
-//
-//  xn = (a * xn−1) mod 2^64
-//  a = 0xf1357aea2e62a9c5
-//  64 output bits
-//
-// TODO: change return to signed i32 like in C
-//
-u32 rand()
-{
-    cur_rand = (0xf1357aea2e62a9c5 * cur_rand);
-
-    // Use the upper 32 bits of the state only
-    return cur_rand >> 32;
-}
-
-void srand(u32 seed)
-{
-    // Seed must be an odd number
-    cur_rand = (seed << 3) + 1;
-}
 
 void main()
 {
     srand(9000);
 
-    u32 N = 50;
+    int N = 50;
 
-    u32 min_val = 0xFFFF;
-    u32 max_val = 0;
-    u32 sum = 0;
+    int min_val = 0xFFFF;
+    int max_val = 0;
+    int sum = 0;
 
     for (int i = 0; i < N; ++i)
     {
-        u32 r = rand() % 10;
+        int r = rand() % 10;
         sum = sum + r;
 
         print_i64(r);
         print_endl();
     }
 
-    u32 avg = sum / N;
+    int avg = sum / N;
     print_str("avg: ");
     print_i64(avg);
     print_endl();
--- ncc/include/stdlib.h
+++ ncc/include/stdlib.h
@@ -6,4 +6,31 @@ void exit(int status)
     asm (status) -> void { exit; };
 }
 
+// The state has 64 bits
+// The seed must be an odd number
+unsigned long __cur_rand__ = 1337;
+
+// Multiplicative Congruential Generator (MCG)
+// Constant from "Computationally Easy, Spectrally Good Multipliers for
+// Congruential Pseudorandom Number Generators" by Steele & Vigna.
+//
+//  xn = (a * xn−1) mod 2^64
+//  a = 0xf1357aea2e62a9c5
+//  64 output bits
+//
+int rand()
+{
+    __cur_rand__ = (0xf1357aea2e62a9c5 * __cur_rand__);
+
+    // Use the upper 32 bits of the state only
+    return (int)(__cur_rand__ >> 32);
+}
+
+void srand(unsigned int seed)
+{
+    // Seed must be an odd number,
+    // So we make sure the lowest bit is 1
+    __cur_rand__ = (seed << 1) + 1;
+}
+
 #endif
