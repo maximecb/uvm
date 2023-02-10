4e2e7a4946b31826b0a059b4e9cb157669b18348
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-02-10 00:14:57 -0500

    Make use of __FILE__ and __LINE__ in assert() macro

--- ncc/include/assert.h
+++ ncc/include/assert.h
@@ -6,13 +6,21 @@
 #define assert(test_val)\
 if (!(test_val))\
 {\
+    asm ("assertion failed in ") -> void { syscall 6; };\
+    asm (__FILE__) -> void { syscall 6; };\
+    asm ("@") -> void { syscall 6; };\
+    asm (__LINE__) -> void { syscall 5; };\
+    asm () -> void { syscall 7; };\
     asm () -> void { panic; };\
 }
 #else
 #define assert(test_val) {}
 #endif
 
-// TODO:
-//todo() macro like in Rust
+// todo() macro for unimplemented features
+#define todo()\
+{\
+    asm ("not yet implemented") -> void { syscall 6; };\
+}
 
 #endif
