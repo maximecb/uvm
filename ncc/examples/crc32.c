e51cf2a64634519f7e3a04e39defc278a000c982
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-03-02 23:13:01 -0500

    Update crc32.c

--- ncc/examples/crc32.c
+++ ncc/examples/crc32.c
@@ -3,13 +3,6 @@ This is the basic CRC-32 calculation with some optimization but no
 table lookup. The the byte reversal is avoided by shifting the crc reg
 right instead of left and by using a reversed 32-bit word to represent
 the polynomial.
-
-When compiled to Cyclops with GCC, this function executes in 8 + 72n
-instructions, where n is the number of bytes in the input message. It
-should be doable in 4 + 61n instructions.
-
-If the inner loop is strung out (approx. 5*8 = 40 instructions),
-it would take about 6 + 46n instructions.
 */
 
 #include <assert.h>
