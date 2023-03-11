30404eef4573252d4e1a3eb0021f5a4684529247
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-03-11 13:53:27 -0500

    WIP printf() implementation

--- ncc/include/stdio.h
+++ ncc/include/stdio.h
@@ -1,10 +1,83 @@
 #ifndef __STDIO_H__
 #define __STDIO_H__
 
+#include <assert.h>
+
 int puts(char* str)
 {
     asm (str) -> void { syscall print_str; };
     return 0;
+}
+
+// Internal buffer used by printf
+char* __buffer[32];
+
+void __write_char(char c)
+{
+    __buffer[0] = c;
+    __buffer[1] = 0;
+    asm (__buffer) -> void { syscall print_str; };
+}
+
+int printf(char* format, ...)
+{
+    unsigned int ch_written = 0;
+    unsigned int var_arg_idx = 0;
+
+    // For each character of the format string
+    for (unsigned int i = 0;; ++i)
+    {
+        char c = format[i];
+
+        if (c == 0)
+        {
+            break;
+        }
+
+        // Format specifier
+        if (c == '%')
+        {
+            if (format[i+1] == '%')
+            {
+                __write_char('%');
+                ++i;
+                ++ch_written;
+                continue;
+            }
+
+            // Decimal integer
+            if (format[i+1] == 'd' || format[i+1] == 'i')
+            {
+                ++i;
+
+                // Get the integer argument and print it
+                /*asm (var_arg_idx) -> void {
+                    get_var_arg;
+                    print_i64;
+                };*/
+                ++var_arg_idx;
+
+                continue;
+            }
+
+            //
+            // TODO: %p, support for printing pointers
+            //
+
+            // Unknown format specifier
+            // Just print it in the output for now
+            // That makes it easier to debug the problem than a panic.
+        }
+
+        // Print this character
+        // NOTE: may want to be careful about printing
+        // in the middle of a unicode character?
+        __write_char(c);
+        ++ch_written;
+    }
+
+    // Return total number of chars written
+    return ch_written;
 }
 
 #endif
--- ncc/tests/printf.c
+++ ncc/tests/printf.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-
-
-
-
+    printf("");
+    printf("Hello!\n");
+    printf("100%%\n");
+    printf("Decimal integer: %d\n", 1337);
 
     return 0;
 }
