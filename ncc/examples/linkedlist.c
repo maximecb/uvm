459d692ff79c2fcd45a7b82e558bd6883caf08a7
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-09-29 18:14:56 -0400

    Add linked list example (not yet working)

---
+++ ncc/examples/linkedlist.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Linked-list node
+typedef struct {
+    int v;
+    node_t* next;
+} node_t;
+
+int main()
+{
+    // FIXME: this doesn't quite work yet
+    /*
+    node_t* list = NULL;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    }
+    */
+
+    return 0;
+}
