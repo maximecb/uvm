6af52c2ac31a90e5a7ad2c4c99d139c1f4075de9
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-10-24 00:04:40 +0100

    Add commented out example code to net.rs

--- vm/src/sys/net.rs
+++ vm/src/sys/net.rs
@@ -1,1 +1,30 @@
+use std::net::{TcpListener, TcpStream};
 use crate::vm::{VM, Value};
+
+
+
+
+
+/*
+fn handle_client(stream: TcpStream) {
+    // ...
+
+    stream.write(&[1])?;
+    stream.read(&mut [0; 128])?;
+
+    //stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+
+
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+*/
+
