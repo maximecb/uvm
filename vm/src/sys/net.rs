7b4623841ced27cce7399c77bdd250404557bc02
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-11-05 14:56:58 -0500

    WIP networking code

--- vm/src/sys/net.rs
+++ vm/src/sys/net.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::os::fd::RawFd;
 use std::thread;
 use std::net::{TcpListener, TcpStream};
@@ -7,17 +7,25 @@ use std::io::{self, Read};
 use std::sync::{Arc, Weak, Mutex};
 use crate::vm::{VM, Value, ExitReason};
 
+// State associated with a socket
+pub struct Socket
+{
+    fd: RawFd,
+
+    /// Incoming connections
+    incoming: VecDeque<TcpStream>,
+
+    // TODO: read buffer
+}
+
 // State for the networking subsystem
 pub struct NetState
 {
-    /// Map of open sockets
-    sockets: HashMap<u64, RawFd>,
-
-    /// Incoming connections
-    incoming: Vec<TcpStream>,
-
     /// Next socket id to use
     next_id: u64,
+
+    /// Map of open sockets
+    sockets: HashMap<u64, Socket>,
 }
 
 impl Default for NetState
@@ -25,11 +33,9 @@ impl Default for NetState
     fn default() -> Self
     {
         Self {
-            sockets: HashMap::default(),
-            incoming: Vec::default(),
-
             // Start at FFFF so we can reserve the low values for error codes
             next_id: 0xFF_FF,
+            sockets: HashMap::default(),
         }
     }
 }
@@ -54,7 +60,12 @@ fn listen_thread(
 
         // Add the new connection to the queue
         let mut net_state = &mut vm.sys_state.net_state;
-        net_state.incoming.push(stream);
+        match net_state.sockets.get_mut(&socket_id) {
+            Some(socket) => {
+                socket.incoming.push_back(stream);
+            }
+            _ => panic!()
+        }
 
         // Call on_new_conn to signal an incoming connection
         match vm.call(on_new_conn, &[Value::from(socket_id)]) {
@@ -78,7 +89,6 @@ pub fn net_listen_tcp(
     ip_space: Value,
     bind_address: Value,
     on_new_conn: Value,
-    flags: Value,
 ) -> Value
 {
     // TODO: accept input address and port
@@ -92,7 +102,13 @@ pub fn net_listen_tcp(
     let mut net_state = &mut vm.sys_state.net_state;
     let socket_id = net_state.next_id;
     net_state.next_id += 1;
-    net_state.sockets.insert(socket_id, socket_fd);
+    net_state.sockets.insert(
+        socket_id,
+        Socket {
+            fd: socket_fd,
+            incoming: VecDeque::default()
+        }
+    );
 
     // Create a listening thread to accept incoming connections
     let vm_mutex = vm.sys_state.mutex.clone();
@@ -112,20 +128,109 @@ pub fn net_listen_tcp(
 
 
 
+
+
+
+
+
+
 // Syscall to accept a new connection
-// Gives you the client address in the buffer you define
-// Will block if there is no incoming connection request
+// Writes the client address in the buffer you specify
 // u64 socket_id = net_accept(u64 socket_id, client_addr_t *client_addr, callback on_incoming_data)
+pub fn net_accept(
+    vm: &mut VM,
+    socket_id: Value,
+    client_addr: Value,
+    client_addr_len: Value,
+    on_incoming_data: Value,
+) -> Value
+{
+    let socket_id = socket_id.as_u64();
+    let client_addr = client_addr.as_u64();
+    let client_addr_len = client_addr_len.as_u64();
+    let on_incoming_data = on_incoming_data.as_u64();
+
+    let mut net_state = &mut vm.sys_state.net_state;
+
+    // If there is a connection waiting
+    match net_state.sockets.get_mut(&socket_id) {
+        Some(socket) => {
+            if socket.incoming.len() == 0 {
+                panic!();
+            }
+
+            let stream = socket.incoming.pop_front().unwrap();
+            let socket_fd = stream.as_raw_fd();
+
+            //
+            // TODO: we need to write the client address into the buffer
+            //
+
+
+
+
+            /*
+            // Assign a socket id to the socket
+            let mut net_state = &mut vm.sys_state.net_state;
+            let socket_id = net_state.next_id;
+            net_state.next_id += 1;
+            net_state.sockets.insert(
+                socket_id,
+                Socket {
+                    fd: socket_fd,
+                    incoming: VecDeque::default()
+                }
+            );
+
+            // Create a listening thread to accept incoming connections
+            let vm_mutex = vm.sys_state.mutex.clone();
+            let on_new_conn = on_new_conn.as_u64();
+            thread::spawn(move || {
+                listen_thread(
+                    vm_mutex,
+                    listener,
+                    socket_id,
+                    on_new_conn,
+                )
+            });
+            */
+
+
+            // Return the socket id
+            //Value::from(socket_id)
+
+
+            todo!();
+
+
+        }
+        _ => panic!()
+    }
+}
 
 // Syscall to read data from a given socket into a buffer you specify
 // u64 num_bytes_read = net_read(u64 socket_id, void* buffer, u64 buf_len)
+pub fn net_read(
+    vm: &mut VM,
+    socket_id: Value,
+    buffer: Value,
+    buf_len: Value,
+) -> Value
+{
+    todo!();
+}
 
 // Syscall to write data on a given socket
 // void net_write(u64 socket_id, void* buffer, u64 buf_len);
-
-
-
-
+pub fn net_write(
+    vm: &mut VM,
+    socket_id: Value,
+    buffer: Value,
+    buf_len: Value,
+) -> Value
+{
+    todo!();
+}
 
 // Syscall to close a socket
 // net_close(u64 socket_id)
@@ -136,67 +241,3 @@ pub fn net_close(
 {
     todo!();
 }
-
-
-
-
-
-
-
-
-
-// We could create a thread for the listening socket that accepts connections,
-// and so simply use this socket in blocking mode
-
-// However, for individual TCP streams, how do we poll?
-// We could use the polling crate: https://crates.io/crates/polling
-
-
-
-/*
-fn handle_client(stream: TcpStream) {
-    // ...
-
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-
-    //stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-
-
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
-
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
-    Ok(())
-}
-*/
-
-
-
-
-
-// Non-blocking read from a TCP stream
-/*
-let mut stream = TcpStream::connect("127.0.0.1:7878")
-    .expect("Couldn't connect to the server...");
-stream.set_nonblocking(true).expect("set_nonblocking call failed");
-
-let mut buf = vec![];
-loop {
-    match stream.read_to_end(&mut buf) {
-        Ok(_) => break,
-        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-            // wait until network socket is ready, typically implemented
-            // via platform-specific APIs such as epoll or IOCP
-            wait_for_fd();
-        }
-        Err(e) => panic!("encountered IO error: {e}"),
-    };
-};
-println!("bytes: {buf:?}");
-*/
