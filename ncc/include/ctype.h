ee1ff9899dfbcbeda7df3f55624216be19c4f752
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-11-10 21:57:41 -0500

    Add isalnum() and isspace() to ctype.h

--- ncc/include/ctype.h
+++ ncc/include/ctype.h
@@ -1,9 +1,32 @@
 #ifndef __CTYPE_H__
 #define __CTYPE_H__
 
+int isalnum(int c)
+{
+    return (
+        (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9')
+    );
+}
+
+// Check if c is a printable character
+// Note that this includes spaces, but excludes \t, \r and \n
 int isprint(int c)
 {
     return (c >= 0x20 && c <= 0x7E);
+}
+
+int isspace(int c)
+{
+    return (
+        c == '\t'   || // 0x09
+        c == '\n'   || // 0x0a
+        c == 0x0b   || // vertical tab, \v
+        c == '\r'   || // 0x0d
+        c == 0x0c   || // form feed, \f
+        c == ' '       // 0x20
+    );
 }
 
 int islower(int c)
