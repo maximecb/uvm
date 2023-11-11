24e26108bb830dc07ee46265e1be802c87351496
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2023-11-11 09:31:03 -0500

    Add struct test

--- ncc/tests/structs.c
+++ ncc/tests/structs.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 // Struct with an array field inside
 typedef struct
 {
@@ -5,7 +7,15 @@ typedef struct
     char name[32];
 } user_t;
 
+typedef struct
+{
+    char buf[32];
+} bufstruct_t;
+
 int main()
 {
+    bufstruct_t* s;
+    assert(sizeof(s->buf) == 32);
+
     return 0;
 }
